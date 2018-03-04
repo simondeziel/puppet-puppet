3bfb865a859a05192ca8320f6fb0bf352d63b74a
Simon Deziel <simon@sdeziel.info>
2018-03-04 00:04:30 -0500

    puppet::agent: allow making the transition from puppet-common to the AIO client

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,61 +1,40 @@
 #
 class puppet::agent (
-  String  $environment         = 'production',
-  Boolean $managed             = true,
-  Boolean $purge_distro_puppet = false,
+  String  $environment = 'production',
+  Boolean $managed     = true,
 ) {
-  if $purge_distro_puppet or $facts['aio_agent_version'] {
-    $cron_command = '/opt/puppetlabs/puppet/bin/puppet agent --onetime --no-daemonize'
-
-    include puppet
-    package { 'puppet-agent':
-      ensure  => installed,
-      require => Class['puppet'],
-    }
-
-    if $purge_distro_puppet {
-      $distro_pkgs = [ 'augeas-lenses',
-                       'facter',
-                       'hiera',
-                       'puppet-common',
-                       'ruby',
-                       'ruby-augeas',
-                       'ruby-rgen',
-                       'ruby-safe-yaml',
-                       'ruby-shadow',
-                     ]
-      package { $distro_pkgs:
-        ensure  => purged,
-        require => Class['puppet'],
-      }
-      file { ['/etc/puppet','/var/lib/puppet']:
-        ensure  => absent,
-        force   => true,
-        require => Class['puppet'],
-      }
+  include puppet
+  package { 'puppet-agent':
+    ensure  => installed,
+    require => [Class['puppet'],Exec['apt_update']],
   }
 
+  # run the puppet agent from cron to save on RAM
+  # and not using mcollective (yet)
+  service { ['puppet','mcollective']:
+    ensure => stopped,
+    enable => false,
+  }
 
-  } else {
-    $cron_command = '[ -x /etc/puppet/puppet-agent.sh ] && /etc/puppet/puppet-agent.sh'
-
-    file { '/etc/puppet/puppet.conf':
-      content => epp('sdeziel/puppet/puppet.conf.epp'),
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
+  if $facts['aio_agent_version'] {
+    $cron_command = '/opt/puppetlabs/puppet/bin/puppet agent --onetime --no-daemonize'
+    if $managed {
+      $cron_ensure = 'present'
+    } else {
+      $cron_ensure = 'absent'
     }
+  } else {
+    # XXX: the legacy client/wrapper script is used one last time to make the transition
+    #      to the AIO/upstream client
+    $cron_command = '[ -x /etc/puppet/puppet-agent.sh ] && /etc/puppet/puppet-agent.sh'
+    $cron_ensure = 'present'
+
     file { '/etc/puppet/puppet-agent.sh':
       mode    => '0755',
-      source  => 'puppet:///modules/sdeziel/puppet/puppet-agent.sh',
+      source  => 'puppet:///modules/puppet/puppet-agent.sh.transition',
     }
   }
 
-  if $managed {
-    $cron_ensure = 'present'
-  } else {
-    $cron_ensure = 'absent'
-  }
   cron { 'puppet-agent':
     ensure   => $cron_ensure,
     user     => 'root',
