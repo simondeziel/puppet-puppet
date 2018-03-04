866fe1dbeda5d2b8df7bbf2542b4a7da4331cdb7
Simon Deziel <simon@sdeziel.info>
2018-03-03 19:21:26 -0500

    puppet: do not manage apt key as we manage the file instead to avoid HTTPS download

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,10 +13,10 @@ class puppet {
     architecture => 'amd64',
     location     => 'http://apt.puppetlabs.com',
     repos        => 'puppet5',
-    key          => {
-      'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
-      #'url'    => 'https://apt.puppetlabs.com/DEB-GPG-KEY-puppet',
-    },
+    #key          => {
+    #  'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
+    #  'url'    => 'https://apt.puppetlabs.com/DEB-GPG-KEY-puppet',
+    #},
     require      => File['/etc/apt/trusted.gpg.d/puppet5-keyring.gpg'],
   }
 }
