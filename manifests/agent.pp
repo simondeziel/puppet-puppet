15601d67c109cbed2dc128be1e0b809cc6b9900f
Simon Deziel <simon@sdeziel.info>
2018-10-13 14:12:58 -0400

    puppet::agent: drop legacy client support

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -25,49 +25,35 @@ class puppet::agent (
     require => Package['puppet-agent'],
   }
 
-  if $facts['aio_agent_version'] {
-    $cron_command = '/opt/puppetlabs/puppet/bin/puppet agent --onetime --no-daemonize'
-    if $managed {
-      $cron_ensure = 'present'
-    } else {
-      $cron_ensure = 'absent'
-    }
+  # AIO puppet.conf path
+  $conf_path = '/etc/puppetlabs/puppet/puppet.conf'
 
-    # AIO puppet.conf path
-    $conf_path = '/etc/puppetlabs/puppet/puppet.conf'
+  # error out when there is an unbound variable
+  ini_setting { 'puppet.conf/main/strict_variables':
+    ensure  => present,
+    path    => $conf_path,
+    section => 'main',
+    setting => 'strict_variables',
+    value   => true,
+  }
+  # disable i18n
+  ini_setting { 'puppet.conf/main/disable_i18n':
+    ensure  => present,
+    path    => $conf_path,
+    section => 'main',
+    setting => 'disable_i18n',
+    value   => true,
+  }
 
-    # error out when there is an unbound variable
-    ini_setting { 'puppet.conf/main/strict_variables':
-      ensure  => present,
-      path    => $conf_path,
-      section => 'main',
-      setting => 'strict_variables',
-      value   => true,
-    }
-    # disable i18n
-    ini_setting { 'puppet.conf/main/disable_i18n':
-      ensure  => present,
-      path    => $conf_path,
-      section => 'main',
-      setting => 'disable_i18n',
-      value   => true,
-    }
-  } else {
-    # XXX: the legacy client/wrapper script is used one last time to make the transition
-    #      to the AIO/upstream client
-    $cron_command = '[ -x /etc/puppet/puppet-agent.sh ] && /etc/puppet/puppet-agent.sh'
-    $cron_ensure = 'present'
-
-    file { '/etc/puppet/puppet-agent.sh':
-      mode    => '0755',
-      source  => 'puppet:///modules/puppet/puppet-agent.sh.transition',
-    }
+  $cron_ensure = $managed ? {
+    true    => 'present',
+    default => 'absent',
   }
 
   cron { 'puppet-agent':
     ensure   => $cron_ensure,
     user     => 'root',
-    command  => $cron_command,
+    command  => '/opt/puppetlabs/puppet/bin/puppet agent --onetime --no-daemonize',
     minute   => fqdn_rand(60),
     hour     =>  [0, 2, '6-18', 22],
     monthday => '*',
